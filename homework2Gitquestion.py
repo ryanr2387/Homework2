992d686ca951b22cc78b241b5435ec09352226bf
Ryan Rayes <rrayes@student.waubonsee.edu>
2019-03-28 22:20:56 -0500

    fixed comflict

--- homework2Gitquestion.py
+++ homework2Gitquestion.py
@@ -2,7 +2,6 @@ num1 = int(input('Enter first number: '))
 num2 = int(input('Enter second number: '))
 
 if num1 > num2:
-<<<<<<< HEAD
     print('Number 1 is bigger than Number 2')
 
 if num2 > num1:
@@ -10,4 +9,3 @@ if num2 > num1:
 
 if num1 == num2:
     print('Number 1 and Number 2 are equal')
->>>>>>> newfeature1
