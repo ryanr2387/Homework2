38b6aaa533e4e5ccccc5f027393f5f96dd3a607d
Ryan Rayes <rrayes@student.waubonsee.edu>
2019-03-28 21:35:27 -0500

    fixed grammar bug

--- homework2Gitquestion.py
+++ homework2Gitquestion.py
@@ -2,4 +2,4 @@ num1 = int(input('Enter first number: '))
 num2 = int(input('Enter second number: '))
 
 if num1 > num2:
-    print('num 1 is bigger than num 2')
+    print('Number 1 is bigger than Number 2')
